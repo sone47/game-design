e4d9678d4a8fe20374443e35d00e68463423fdcc
sone <sone47.china@gmail.com>
2018-08-05 09:29:03 +0800

    增加render方法

--- script.js
+++ script.js
@@ -1,87 +1,91 @@
 const NUMBER_RANGE = 100;
-    const CHANCES_NUMBER = 10;
-    const TIPS = ['Too low!', 'Correct!', 'Too high!'];
+const CHANCES_NUMBER = 10;
+const TIPS = ['Too low!', 'Correct!', 'Too high!'];
 
-    const guessBtn = document.querySelector('#guess-btn');
-    const startBtn = document.querySelector('#start-btn');
-    const input = document.querySelector('#input');
-    const tip = document.querySelector('#tip');
+const guessBtn = document.querySelector('#guess-btn');
+const startBtn = document.querySelector('#start-btn');
+const input = document.querySelector('#input');
+const tip = document.querySelector('#tip');
 
-    startBtn.addEventListener('click', start);
+startBtn.addEventListener('click', start);
 
-    function onGuess() {
-      const inputValue = Number(input.value);
-      remainChances = remainChances - 1;
-      let tipValue = '';
+function onGuess() {
+  const inputValue = Number(input.value);
+  remainChances = remainChances - 1;
+  let tipValue = '';
 
-      if(remainChances) {
-        tipValue = 'You remain ' + remainChances + ' chances.';
+  if(remainChances) {
+    tipValue = 'You remain ' + remainChances + ' chances.';
 
-        if(validateInput(inputValue)) {
-          if(inputValue < randomNumber) {
-            tipValue = TIPS[0] + tipValue;
-          } else if(inputValue > randomNumber) {
-            tipValue = TIPS[2] + tipValue;
-          } else if(inputValue === randomNumber) {
-            tipValue = TIPS[1];
-            gameOver(true);
-          }
-        } else {
-          tipValue = 'Input should be a number. ' + 'You remain ' + remainChances + ' chances.';
-        }
-      } else {
-        tipValue = 'Opps! You missed the last chance! The mystery number is ' + randomNumber + '. You can restart and guess again.';
-        gameOver(false);
+    if(validateInput(inputValue)) {
+      if(inputValue < randomNumber) {
+        tipValue = TIPS[0] + tipValue;
+      } else if(inputValue > randomNumber) {
+        tipValue = TIPS[2] + tipValue;
+      } else if(inputValue === randomNumber) {
+        tipValue = TIPS[1];
+        gameOver(true);
       }
-
-      tip.textContent = tipValue;
+    } else {
+      tipValue = 'Input should be a number. ' + 'You remain ' + remainChances + ' chances.';
     }
+  } else {
+    tipValue = 'Opps! You missed the last chance! The mystery number is ' + randomNumber + '. You can restart and guess again.';
+    gameOver(false);
+  }
 
-    function clickHanlder() {
-      onGuess();
-    }
+  render(tipValue);
+}
 
-    function keyDownHandler(e) {
-      if(e.keyCode === 13) {
-        onGuess();
-      }
-    }
+function clickHanlder() {
+  onGuess();
+}
 
-    function start() {
-      randomNumber = getRandomNumber();
-      remainChances = CHANCES_NUMBER;
-      clearText();
-      enableAll();
-    }
+function keyDownHandler(e) {
+  if(e.keyCode === 13) {
+    onGuess();
+  }
+}
 
-    function getRandomNumber() {
-      return Math.ceil(Math.random() * NUMBER_RANGE);
-    }
+function start() {
+  randomNumber = getRandomNumber();
+  remainChances = CHANCES_NUMBER;
+  clearText();
+  enableAll();
+}
 
-    function gameOver(won) {
-      disableAll();
-    }
+function getRandomNumber() {
+  return Math.ceil(Math.random() * NUMBER_RANGE);
+}
 
-    function clearText() {
-      tip.textContent = '';
-      input.value = '';
-    }
+function gameOver(won) {
+  disableAll();
+}
 
-    function enableAll() {
-      guessBtn.disabled = false;
-      input.disabled = false;
-      input.focus();
-      guessBtn.addEventListener('click', clickHanlder);
-      input.addEventListener('keydown', keyDownHandler);
-    }
+function clearText() {
+  tip.textContent = '';
+  input.value = '';
+}
 
-    function disableAll() {
-      guessBtn.disabled = true;
-      input.disabled = true;
-      guessBtn.removeEventListener('click', clickHanlder);
-      input.removeEventListener('keydown', keyDownHandler);
-    }
+function enableAll() {
+  guessBtn.disabled = false;
+  input.disabled = false;
+  input.focus();
+  guessBtn.addEventListener('click', clickHanlder);
+  input.addEventListener('keydown', keyDownHandler);
+}
 
-    function validateInput(input) {
-      return !isNaN(input);
-    }
+function disableAll() {
+  guessBtn.disabled = true;
+  input.disabled = true;
+  guessBtn.removeEventListener('click', clickHanlder);
+  input.removeEventListener('keydown', keyDownHandler);
+}
+
+function validateInput(input) {
+  return !isNaN(input);
+}
+
+function render(tipValue) {
+  tip.textContent = tipValue;
+}
